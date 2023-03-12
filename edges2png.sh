6ac7cb088b3b83d983a0b61c97c4d26a94d7ba5c
hayden1126 <haydenleung1126@gmail.com>
2023-03-12 14:26:12 +0000

    made a more tidy system

--- edges2png.sh
+++ edges2png.sh
@@ -1,18 +1,20 @@
+#!/bin/sh
 EDGESFILE=$1
 FILEPREFIX=${EDGESFILE%.*}
 LGLPATH=$2
+DIRPATH=./visualisation/$FILEPREFIX
 
-if [ ! -f EDGESFILE ]; then
-    echo Edges file: $EDGESFILE not found
-    exit 1
-fi
+# if [ ! -f EDGESFILE ]; then
+#     echo Edges file: $EDGESFILE not found
+#     exit 1
+# fi
 
-mkdir -p $FILEPREFIX
-julia edges2lgl.jl $EDGESFILE ./$FILEPREFIX/$FILEPREFIX.lgl
-julia edges2labels.jl $EDGESFILE ./id_titles/titles.txt 3 ./$FILEPREFIX/$FILEPREFIX.labels
-julia edges2colors.jl $EDGESFILE ./$FILEPREFIX/$FILEPREFIX.colors
+mkdir -p $DIRPATH
+julia edges2lgl.jl $EDGESFILE $DIRPATH/$FILEPREFIX.lgl
+julia edges2labels.jl $EDGESFILE ./id_titles/titles.txt 3 $DIRPATH/$FILEPREFIX.labels
+julia edges2colors.jl $EDGESFILE $DIRPATH/$FILEPREFIX.colors
 
-$LGLPATH/bin/lglayout2D -t 4 -L -e -l $FILEPREFIX/$FILEPREFIX.lgl
-mv $LGLPATH/lgl.out* $FILEPREFIX/
-java -Djava.awt.headless=false -Xmx8G -Xms6G -cp $LGLPATH/Java/jar/LGLLib.jar ImageMaker.GenerateImages 10000 10000 $FILEPREFIX/$FILEPREFIX.lgl $FILEPREFIX/lgl.out -c $FILEPREFIX/$FILEPREFIX.colors -l ./$FILEPREFIX/$FILEPREFIX.labels -s 0.01
-for f in $FILEPREFIX/lgl.out_10000x10000*.png; do mv "$f" "$(echo "$f" | sed s/lgl.out_10000x10000/${FILEPREFIX}_10k/)"; done
+$LGLPATH/bin/lglayout2D -t 4 -L -e -l $DIRPATH/$FILEPREFIX.lgl
+mv ./lgl.out* $DIRPATH/
+java -Djava.awt.headless=false -Xmx8G -Xms6G -cp $LGLPATH/Java/jar/LGLLib.jar ImageMaker.GenerateImages 10000 10000 $DIRPATH/$FILEPREFIX.lgl $DIRPATH/lgl.out -c $DIRPATH/$FILEPREFIX.colors -l $DIRPATH/$FILEPREFIX.labels -s 0.01
+# for f in $FILEPREFIX/lgl.out_10000x10000*.png; do mv "$f" "$(echo "$f" | sed s/lgl.out_10000x10000/${FILEPREFIX}_10k/)"; done
--- kedges2png.sh
+++ kedges2png.sh
@@ -1,5 +1,9 @@
+#!/bin/sh
 K=$1
 LGLPATH=$2
+DIRPATH=./visualisation/${K}-core
+
+mkdir -p $DIRPATH
 
 if [ ! -f ./k_edges/${K}_core_edges.txt ]; then
     echo Edges file: ./k_edges/${K}_core_edges.txt not found
@@ -7,22 +11,14 @@ if [ ! -f ./k_edges/${K}_core_edges.txt ]; then
 fi
 
 if [ ! -f ./LGLfiles/${K}_core_edges.lgl ]; then
-    julia edges2lgl.jl ./k_edges/${K}_core_edges.txt ./LGLfiles/${K}_core_edges.lgl
+    julia edges2lgl.jl ./k_edges/${K}_core_edges.txt $DIRPATH/${K}_core_edges.lgl
 fi
 
 edgesfile=./k_edges/${K}_core_edges.txt
-julia edges2labels.jl $edgesfile ./id_titles/titles.txt 3 ./colorlabels/${K}.labels
-julia edges2colors.jl $edgesfile ./colorlabels/${K}.colors
-mkdir -p $LGLPATH/${K}-core
-cp ./colorlabels/${K}.labels $LGLPATH/${K}-core/
-cp ./colorlabels/${K}.colors $LGLPATH/${K}-core/
+julia edges2labels.jl $edgesfile ./id_titles/titles.txt 3 $DIRPATH/${K}.labels
+julia edges2colors.jl $edgesfile $DIRPATH/${K}.colors
 
-if [ ! -f $LGLPATH/${K}-core/${K}_core_edges.lgl ]; then
-    cp ./LGLfiles/${K}_core_edges.lgl $LGLPATH/${K}-core/
-fi
-
-cd $LGLPATH
-./bin/lglayout2D -t 4 -L -e -l ${K}-core/${K}_core_edges.lgl
-mv lgl.out* ${K}-core/
-java -Djava.awt.headless=false -Xmx8G -Xms6G -cp ./Java/jar/LGLLib.jar ImageMaker.GenerateImages 10000 10000 ./${K}-core/${K}_core_edges.lgl ./${K}-core/lgl.out -c ./${K}-core/${K}.colors -l ./${K}-core/${K}.labels -s 0.01
-for f in ./${K}-core/lgl.out_10000x10000*.png; do mv "$f" "$(echo "$f" | sed s/lgl.out_10000x10000/${K}_core_10k/)"; done
+$LGLPATH/bin/lglayout2D -t 4 -L -e -l $DIRPATH/${K}_core_edges.lgl
+mv lgl.out* $DIRPATH
+java -Djava.awt.headless=false -Xmx8G -Xms6G -cp $LGLPATH/Java/jar/LGLLib.jar ImageMaker.GenerateImages 10000 10000 $DIRPATH/${K}_core_edges.lgl $DIRPATH/lgl.out -c $DIRPATH/${K}.colors -l $DIRPATH/${K}.labels -s 0.01
+for f in $DIRPATH/lgl.out_10000x10000*.png; do mv "$f" "$(echo "$f" | sed s/lgl.out_10000x10000/${K}_core_10k/)"; done

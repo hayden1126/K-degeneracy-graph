3e91fa2eea0f90fbf09dd37f2c09ca104e89e5a1
hayden1126 <haydenleung1126@gmail.com>
2023-03-12 15:18:38 +0000

    tweaked parameters

--- kcorestats.jl
+++ kcorestats.jl
@@ -2,25 +2,27 @@ include("EdgeUtils.jl")
 using ProgressBars, .ReadUtils, .LinksUtils, .StatUtils
 
 function read_kcorenums(filename::String)
-    cores = Dict{Int32, Int32}()
+    kcores = Int32[]
+    degrees = Int32[]
     open(filename) do f
         for line in eachline(f)
             pair = split(line)
-            cores[parse(Int32, pair[1])] = parse(Int32, pair[2])
+            push!(kcores, parse(Int32, pair[1]))
+            push!(degrees, parse(Int32, pair[2]))
         end
     end
-    return cores
+    return kcores, degrees
 end
 
 function main()
-    cores = read_kcorenums(EDGESFILE)
-    scat(values(cores), keys(cores), "output/k_corenums.png", "Relationship between K-core number and number of nodes of Wikipedia Pages")
+    kcores, degrees = read_kcorenums(EDGESFILE)
+    scat(kcores, degrees, "output/k_corenums.png", "Relationship between K-core number and number of nodes"; xlab="K-core", ylab="Degree")
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     if length(ARGS) != 1 || !isfile(ARGS[1])
         println("Usage: julia kcorestats.jl <edgesfile>")
     end 
-    EDGESFILE = ARGS[1]
+    const EDGESFILE = ARGS[1]
     main()
 end

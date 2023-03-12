9d529f8b85b7820e00b15925a5005f0f6e29ea99
hayden1126 <haydenleung1126@gmail.com>
2023-03-12 14:24:09 +0000

    implemented .LglUtils module

--- edges2lgl.jl
+++ edges2lgl.jl
@@ -1,41 +1,8 @@
-function write2LGL(edgesfile::String, outputfile::String)
-    print("Converting to LGL format... [0.0%]\r")
-    filelines = countlines(edgesfile)
-
-    prime = split(readline(edgesfile))[1]
-    primelinks = String[]
-
-    # For each line in edges file
-    for (index, line) = enumerate(eachline(edgesfile))
-
-        pair = split(line)
-
-        if pair[1] == prime
-            push!(primelinks, pair[2])
-        else
-            # Write to file
-            open(outputfile, "a") do f
-                println(f, "# $prime\n$(join(primelinks, '\n'))")
-            end
-            prime = pair[1]
-            primelinks = String[pair[2]]
-        end
-
-        # Print progress
-        if index % 1000000 == 0
-            print("Converting to LGL format... [$(round(index/filelines*100, digits=1))%]\r")
-        end
-    end
-
-    # Process the last prime node
-    open(outputfile, "a") do f
-        println(f, "# $prime\n$(join(primelinks, '\n'))")
-    end
-    println("Converting to LGL format... [100%] !")    
-end
+include("EdgeUtils.jl")
+using .LglUtils
 
 function main()
-    write2LGL(ARGS[1], ARGS[2])
+    write_edges2lgl(EDGESFILE, OUTPUTFILE)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
@@ -43,5 +10,7 @@ if abspath(PROGRAM_FILE) == @__FILE__
         println("Usage: julia edges2lgl.jl <edgesfile> <outputfile>")
         exit(1)
     end
+    const EDGESFILE = ARGS[1]
+    const OUTPUTFILE = ARGS[2]
     main()
 end
